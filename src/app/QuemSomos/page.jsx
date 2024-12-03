5dc99dd66a5514590c7bf9961d994c954e03e4c8
Fernando Neto <berolexjr22@gmail.com>
2024-12-03 17:05:55 -0300

    pag quem somos

--- src/app/QuemSomos/page.jsx
+++ src/app/QuemSomos/page.jsx
@@ -16,7 +16,8 @@ export default function QuemSomos() {
               <p>Cadastre-se</p>
             </div>
 
-            <div className={styles.inicio}>
+            <div className={styles.inicio}> 
+              
               <p>Início</p>
             </div>
 
@@ -34,12 +35,16 @@ export default function QuemSomos() {
 
           </div>
 
+          <div className={styles.linha}>
+
+          </div>
+
           <div className={styles.anuncio}>
 
           </div>
         </div>
 
-        <div className={styles.sobre}>
+        <div className={styles.sobre_dmark}>
           <div className={styles.logo}>
             <p>logo</p>
           </div>
@@ -47,15 +52,20 @@ export default function QuemSomos() {
           <h1>Quem Somos?</h1>
 
           <p>
-              Dmark
+               <br /><br />
 
-            Na Dmark, acreditamos que o tempo é um dos nossos bens mais preciosos. Nossa missão é facilitar a gestão do seu   tempo, proporcionando uma plataforma de agendamento de horários simples, eficiente e intuitiva. 
+            Na Dmark, acreditamos que o tempo é um dos nossos bens mais preciosos. Nossa missão é facilitar a gestão do seu   tempo, proporcionando uma plataforma de agendamento de horários simples, eficiente e intuitiva. <br /><br />
     
             Fundada por uma equipe apaixonada por inovação e tecnologia, a Dmark surgiu da necessidade de transformar a forma   como as pessoas e empresas marcam compromissos. Com uma interface amigável e funcionalidades que atendem a diversas   necessidades, nosso objetivo é garantir que cada agendamento seja feito de maneira rápida e sem complicações.
+            <br /><br />
 
             Oferecemos soluções personalizadas que se adaptam a diferentes setores, desde serviços de saúde até consultas   empresariais. Na Dmark, a prioridade é a satisfação do cliente, e estamos sempre em busca de novas maneiras de  aprimorar nossa plataforma para atender às demandas do mercado.
+            
+            <br /><br />
 
             Nosso compromisso vai além da tecnologia: valorizamos o relacionamento humano e a confiança. Acreditamos que, ao  otimizar o seu tempo, podemos contribuir para que você se concentre no que realmente importa.
+
+            <br /><br />
 
             Seja bem-vindo à Dmark, onde cada minuto conta!
 
