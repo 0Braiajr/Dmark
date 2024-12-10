46637b1e41536cfa070eb047c74ddd13b6a100ef
Fernando Neto <berolexjr22@gmail.com>
2024-12-10 16:53:53 -0300

    header/footer 10/12

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,10 +1,32 @@
 import styles from "../styles/footer.module.css"
 
+
 export default function footer() {
   
   return(
-    <div className={styles.body}>
-        <p>Olá</p>
+    <div className={styles.footer}>
+      <div className={styles.logo}>
+        LOGO
+      </div>
+
+      <div className={styles.infos}>
+        <span>CONTATE A DMARK</span> 
+        <p>TELEFONE: (00) 0 0000-0000</p>
+        <p>EMAIL: emailDmark00@gmail.com</p>
+        <p>NOS SIGA</p>
+        <div className={styles.logos_info}>
+          <div className={styles.social_media}>LINKEDIN</div>
+          <div className={styles.social_media}>INSTAGRAM</div>
+        </div>
+
+        <div className={styles.developers}>
+          <p>DESENVOLVIDO POR: MARCELO VYNICIUS / FERNANDO NETO / JOÃO LEVI</p>
+        </div>
+      </div>
+      
+      
+      
+
     </div>
   );
 }
--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -2,8 +2,30 @@ import styles from "../styles/header.module.css"
 
 export default function header() {
     return(
-        <div className={styles.body}>
-            <p>Olá</p>
+        <div className={styles.header}>
+            <div className={styles.menu_button}>
+                <a href="">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </a>
+            </div>
+
+            <div className={styles.logo}>
+                LOGO
+            </div>
+
+            <div className={styles.search}>
+
+                <input type="text" name="" id="" placeholder="PESQUISAR (BARBEARIAS)" className={styles.search_bar}/>
+
+                
+
+                
+            </div>
+            
+            
+
         </div>
     );
 }
